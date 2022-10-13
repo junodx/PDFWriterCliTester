e124ad5eb746973b70b080ede18cb2d55f2f3e7e
arun <arun@bnma.us>
2022-10-13 17:54:15 +0530

    PLAT-401

--- src/main/java/SimplePDFWriterTestMain.java
+++ src/main/java/SimplePDFWriterTestMain.java
@@ -1,6 +1,7 @@
 import com.junodx.api.models.auth.User;
 import com.junodx.api.models.laboratory.TestReport;
 import com.junodx.api.models.laboratory.reports.NIPSReportStrings;
+import com.junodx.api.models.laboratory.reports.RawData;
 import com.junodx.api.models.laboratory.reports.nipsstrings.Condition;
 import com.lowagie.text.DocumentException;
 import de.neuland.pug4j.Pug4J;
@@ -14,6 +15,8 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+
+import org.json.JSONObject;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.itextpdf.html2pdf.HtmlConverter;
@@ -37,7 +40,6 @@ public class SimplePDFWriterTestMain {
             if(report == null)
                 return;
 
-      System.out.println("Report: " + mapper.writeValueAsString(report));
             NIPSReportStrings strings = (NIPSReportStrings) report.getResultData().getStrings();
 
             User patient = report.getPatient();
@@ -46,6 +48,9 @@ public class SimplePDFWriterTestMain {
                 return;
             }
             
+            RawData status = report.getResultData().getData();
+            JSONObject statusJson = new JSONObject(status);
+
             Date signedOutAt = report.getSignoutDetails().getSignedOutAt().getTime();
             Date lmp = patient.getPatientDetails().getMedicalDetails().getLmpDate().getTime();
             Date collection = null;
@@ -53,86 +58,79 @@ public class SimplePDFWriterTestMain {
                 collection = report.getSampleCollectionTimestamp().getTime();
 
             Map<String, Object> obj = new HashMap<>();
+            obj.put("overallCondition", statusJson.get("overallCondition").toString());
             obj.put("patientName", patient.getFirstName() + " " + patient.getLastName());
             obj.put("patientDob", patient.getDateOfBirth());
             obj.put("patientEmail", patient.getEmail());
-            obj.put("reportDate", sdf.format(signedOutAt));
-            obj.put("productName", "Positive Result for Hazel NIPS Basic");
-            obj.put("testNameShort", strings.getTestDetails().getTestShortName());
             obj.put("patientLmpDate", sdf.format(lmp));
             obj.put("patientSampleCollectionDate", collection != null ? sdf.format(collection) : "");
             obj.put("patientGA", strings.getPpvDetails().getPpvPatientGA());
             obj.put("patientGestationType", "Singleton");
+            obj.put("reportDate", sdf.format(signedOutAt));
+
+            obj.put("productName", "Positive Result for Hazel NIPS Basic");
+            obj.put("testNameShort", strings.getTestDetails().getTestShortName());
             obj.put("testNameExtended", strings.getTestDetails().getTestName());
+            obj.put("testName", strings.getTestDetails().getTestName());
             obj.put("testDescription", strings.getTestDetails().getTestDescription());
+            
             obj.put("overallResultText", strings.getOverallResults().getOverallResultText());
             obj.put("overallResultSVGPdf", strings.getOverallResults().getOverallResultSVGPdf());
             obj.put("fetalSexText", strings.getOverallResults().getOverallResultFetalSexText());
             obj.put("overallFetalSexSVGPdf", strings.getOverallResults().getOverallFetalSexSVGPdf());
             obj.put("fetalFractionText", strings.getOverallResults().getOverallResultFetalFractionText());
             obj.put("overallFetalFractionSVGPdf", strings.getOverallResults().getOverallFetalFractionSVGPdf());
+            
             obj.put("overallResultsSummary", strings.getOverallResults().getOverallResultsSummary());
-            
-            obj.put("patientDemographics", "PATIENT DEMOGRAPHICS");
-            obj.put("testNameExtended", "ABOUT JUNO'S HAZEL™ NON - INVASIVE PRENATAL SCREEN:");
-            obj.put("testDescription", "Juno Diagnostics' Hazel™ laboratory-developed test (LDT) is a screening evaluation which analyzes circulating cell-free DNA from a maternal blood sample for chromosomal and subchromosomal representations of the fetus and gestational carrier. The screen is indicated for use in human pregnancies for the screening of fetal chromosomal aberrations. Validation data on multiple pregnancies, such as twins, is limited and the ability of this screen to detect aneuploidy in a triplet pregnancy has not yet been validated.");
+            obj.put("overallResult", status);
 
-            obj.put("FINAL_TEST_SUMMARY_HEADER", "FINAL RESULTS SUMMARY");
-            obj.put("RESULT", "Result");
-            obj.put("FETAL_SEX", "Fetal Sex");
-            obj.put("FETAL_FRACTION", "Fetal Fraction");
-
-            obj.put("POSITIVE_PREDICTIVE_VALUE_HEADER", "POSITIVE PREDICTIVE VALUE");
-            // obj.put("predictivePositiveValue", strings.getPpvDetails().getPpvPercent());
-            obj.put("RESULT", "Result");
-            
             // PPV Section
             obj.put("ppvPercentage", strings.getPpvDetails().getPpvPercent());
             obj.put("ppvPatientAge", strings.getPpvDetails().getPpvPatientAge());
             obj.put("ppvPatientGA", strings.getPpvDetails().getPpvPatientGA());
             obj.put("ppvExplanation", strings.getPpvDetails().getPpvExplanation());
 
-
-            
-            obj.put("POST_TEST_RISK", "Post-test risk");
-            obj.put("INTERPRETATION", "Interpretation");
-
-
-            obj.put("WHAT_DOES_THIS_RESULT_MEAN_HEADER", "WHAT DOES THIS RESULT MEAN?");
-            
-            obj.put("SCREENING_METHODS_HEADER", "SCREENING METHODS");
-            obj.put("screeningMethodTest", "Circulating cell-free DNA (ccfDNA) is purified from the plasma component of maternal blood. The extracted DNA is then converted into a whole genome DNA library for sequencing-based analysis of chromosomes 21, 18, and 13.");
-            obj.put("SCREENING_PERFORMANCE_HEADER", "SCREENING PERFORMANCE");
-            obj.put("screeningPerformaceText", "Juno’s Hazel™ laboratory developed test has been evaluated for clinical performance in multiple studies, inclusive of >1600 total venous and capillary samples. Based on this data, expected performance with at-home selfcollection is as follows:");
-
-
-            obj.put("SCREENING_LIMITATIONS_HEADER", "SCREENING LIMITATIONS");
-
             // Conditions section
             int count = 1;
-            for(Condition condition : strings.getConditions()) {
+            for (Condition condition : strings.getConditions()) {
                 obj.put("condition" + count + "Title", condition.getLabel());
                 obj.put("condition" + count + "Risk", condition.getText());
                 obj.put("condition" + count + "InterpretationText", condition.getDetectionString());
                 obj.put("condition" + count + "Icon", condition.getIcon());
                 count++;
-                //The fetal sex is included in the conditions list, need to ignore that explicitly for this report
             }
-                 /* 
+            
+            //obj.put("testNameExtended", "ABOUT JUNO'S HAZEL™ NON - INVASIVE PRENATAL SCREEN:"); //to-do
+            obj.put("patientDemographics", "PATIENT DEMOGRAPHICS");
+            
+            // Table Headers
+            obj.put("CONDITIONS_EVALUATED_HEADER", "CONDITIONS EVALUATED");
+            obj.put("FINAL_TEST_SUMMARY_HEADER", "FINAL RESULTS SUMMARY");
+            obj.put("POSITIVE_PREDICTIVE_VALUE_HEADER", "POSITIVE PREDICTIVE VALUE");
+            obj.put("WHAT_DOES_THIS_RESULT_MEAN_HEADER", "WHAT DOES THIS RESULT MEAN?");
+            obj.put("SCREENING_METHODS_HEADER", "SCREENING METHODS");
+            obj.put("SCREENING_PERFORMANCE_HEADER", "SCREENING PERFORMANCE");
+            obj.put("SCREENING_LIMITATIONS_HEADER", "SCREENING LIMITATIONS");
+            obj.put("SCREENING_REFERENCES_HEADER", "REFERENCES" );
 
+            // table rows static text
+            obj.put("RESULT", "Result");
+            obj.put("FETAL_SEX", "Fetal Sex");
+            obj.put("FETAL_FRACTION", "Fetal Fraction");
+            obj.put("POST_TEST_RISK", "Post-test risk");
+            obj.put("INTERPRETATION", "Interpretation");
+            
             /*
-             * Results meaning section
-             * obj.put("resultsMeaning", strings.getOverallResults().getOverallResultsSummary());
-               */
+              Results meaning section
+              obj.put("resultsMeaning", strings.getOverallResults().getOverallResultsSummary());
+            */
 
             // Static text section
-            obj.put("screeningPerformance", strings.getStaticText().getScreeningPerformance());
-            obj.put("screeningLimitations", strings.getStaticText().getScreeningLimitations());
-            obj.put("screeningReferences", "REFERENCES" );
+            obj.put("screeningPerformaceText", strings.getStaticText().getScreeningPerformance());
+            obj.put("screeningMethodText", strings.getStaticText().getScreeningMethods());
             obj.put("disclaimer", strings.getStaticText().getDisclaimer().replaceAll("’", "'"));
             obj.put("hipaaDisclaimer", strings.getStaticText().getHipaaDisclaimer());
             obj.put("junoFooter", strings.getStaticText().getJunoFooter());
-            obj.put("testName", "JUNO: HAZEL™ NON-INVASIVE PRENATAL SCREEN BASIC");
 
             PugConfiguration config = new PugConfiguration();
             config.setMode(Pug4J.Mode.HTML);
@@ -144,7 +142,6 @@ public class SimplePDFWriterTestMain {
             html = html.replace("[screeningLimitations]", screeningLimitations);
 
             System.out.println("HTML: " + mapper.writeValueAsString(html));
-            System.out.println("strings: " + mapper.writeValueAsString(strings.getStaticText().getDisclaimer()));
             
             HtmlConverter.convertToPdf(html, new FileOutputStream(obj.get("patientName") + ".pdf"));
         } catch (URISyntaxException e) {
